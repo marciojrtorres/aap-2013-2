d13fb3d7a0fed06f644f5826921c9a32c97c4cad
Marcio Torres <marciojrtorres@gmail.com>
2013-08-27 21:01:13 -0300

    projeto intro

---
+++ intro/src/intro/Main.java
@@ -0,0 +1,21 @@
+package intro;
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] argumentos) {
+    
+    	Scanner scan = new Scanner(System.in);
+
+    	System.out.println("Primeiro: ");
+        String s1 = scan.nextLine();
+        System.out.println("Segundo: ");
+        String s2 = scan.nextLine();
+
+        System.out.println(s1);
+        System.out.println(s2);
+
+    }   
+
+}
