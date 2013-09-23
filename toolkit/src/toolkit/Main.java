2b02efd67b0b8edb3f6859848e714e9b917f69a1
Marcio Torres <marciojrtorres@gmail.com>
2013-09-23 16:25:15 -0300

    randomizador main

--- toolkit/src/toolkit/Main.java
+++ toolkit/src/toolkit/Main.java
@@ -1,13 +1,16 @@
 package toolkit;
 
-// import estÃ¡tico: importaÃ§Ã£o dos membros da classe
 import static java.lang.System.out;
 import static toolkit.StringUtil.*;
 
 public class Main {
     public static void main(String[] argumentos) {
-        for (int i = 0; i < 100; i++)
+
+        for (int i = 0; i < 100; i++) {
+
             out.println((int)(Randomizer.random() * 6 + 1));
+            
+        }
         
     }   
 }
