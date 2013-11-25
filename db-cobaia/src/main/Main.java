0e2903c568657de9d763d452b158e19979edada2
Marcio Torres <marciojrtorres@gmail.com>
2013-11-25 14:15:21 -0200

    alterado

--- db-cobaia/src/main/Main.java
+++ db-cobaia/src/main/Main.java
@@ -11,14 +11,15 @@ public class Main {
 		Class.forName("com.mysql.jdbc.Driver");
 		
 		// #2 Conectar
-		Connection con = DriverManager.getConnection("jdbc:mysql://localhost/cobaia",
-				                    "root", "root");
+		Connection con = DriverManager
+				.getConnection("jdbc:mysql://localhost/cobaia", "root", "root");
 		
 		// #3 Criar um comando
 		Statement cmd = con.createStatement();
 		
 		// #4 Executar o comando
-		cmd.execute("INSERT INTO contatos (nome, sobrenome, telefone, email) VALUES ('Bruno', 'Lima', '32345566', 'bruno.lima@furg.br');");
+		cmd.execute("INSERT INTO contatos (nome, sobrenome, telefone, email) "
+			      + "VALUES ('Bruno', 'Lima', '32345566', 'bruno.lima@furg.br');");
 		
 		// #5 Fechar tudo
 		cmd.close();
